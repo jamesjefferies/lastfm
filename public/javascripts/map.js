2d6403f3d495c47b0abe192cb81a4f944e45b4a9
James Jefferies <james@shedcode.co.uk>
2014-03-28 11:19:15 +0000

    Messing about and adding some words

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,28 +1,33 @@
 var SOUNDMAP = {
 
-  json_data: [],
+  map: null,
   getData: function() {
+
+    SOUNDMAP.map = SOUNDMAP.loadMap();
+
     $.getJSON("/data.json", function( data ) {
-        SOUNDMAP.json_data = data;
-        SOUNDMAP.loadMap(data);
+        SOUNDMAP.addMarkers(data);
       });
   },
-  loadMap: function(data) {
+  loadMap: function() {
 
-    var map = L.map('map').setView([51.5, -0.09], 7);
+    var map = L.map('map');
+    map.setView([51.5, -0.09], 7);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-      var array = data;
+    return map;
 
-      for (var count in array) {
-        SOUNDMAP.addMarker(L, map, array[count]);
-      }
-    },
+  },
+  addMarkers: function(data) {
+    for (var count in data) {
+      SOUNDMAP.addMarker(L, data[count]);
+    }
+  },
 
-    addMarker: function(L, map, m) {
+  addMarker: function(L, m) {
       var LeafIcon = L.Icon.extend({
         options: {
           // iconSize:     [38, 95],
@@ -36,7 +41,7 @@ var SOUNDMAP = {
       var popUpText = m.top_artist + " is the favourite in: " + m.metro;
 
       var icon = new LeafIcon({iconUrl: m.image_url});
-      L.marker([m.lat, m.long], {icon: icon}).bindPopup(popUpText).addTo(map);
+      L.marker([m.lat, m.long], {icon: icon}).bindPopup(popUpText).addTo(SOUNDMAP.map);
     }
 }
 window.onload = SOUNDMAP.getData();
